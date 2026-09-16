e8072db619d433a98c49c85e0ec869f862153353
Matthias Hester <72701890+brainscan1980@users.noreply.github.com>
2026-09-16 12:18:06 +0200

    Initialize historical statistics module

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,9 @@
 import app as core
 from eventlog import init_eventlog
 from cluster_integration import init_cluster_validation
+from statistics import init_statistics
 
 init_eventlog(core)
 init_cluster_validation(core)
+init_statistics(core)
 app=core.app
