198192b89cf8d17c38e8d3b39b1ec5d665b1102e
Matthias Hester <72701890+brainscan1980@users.noreply.github.com>
2026-09-16 14:03:15 +0200

    Initialize audit log foundation

--- app/wsgi.py
+++ app/wsgi.py
@@ -5,10 +5,12 @@ from statistics import init_statistics
 from node_actions import init_node_actions
 from config_transfer import init_config_transfer
 from failover_test import init_failover_test
+from audit_log import init_audit_log
 
 init_eventlog(core)
 init_cluster_validation(core)
 init_statistics(core)
+init_audit_log(core)
 init_node_actions(core)
 init_config_transfer(core)
 init_failover_test(core)
