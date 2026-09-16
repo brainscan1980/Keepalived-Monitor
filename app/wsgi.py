5bd02ada0ea44e742d7e1bf2b35bb4059ef3bdb8
Matthias Hester <72701890+brainscan1980@users.noreply.github.com>
2026-09-16 13:28:44 +0200

    Initialize configuration transfer module

--- app/wsgi.py
+++ app/wsgi.py
@@ -3,9 +3,11 @@ from eventlog import init_eventlog
 from cluster_integration import init_cluster_validation
 from statistics import init_statistics
 from node_actions import init_node_actions
+from config_transfer import init_config_transfer
 
 init_eventlog(core)
 init_cluster_validation(core)
 init_statistics(core)
 init_node_actions(core)
+init_config_transfer(core)
 app=core.app
