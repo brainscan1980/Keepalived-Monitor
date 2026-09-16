04f97c52ee381528f13d2b806d109a427c1dab43
Matthias Hester <72701890+brainscan1980@users.noreply.github.com>
2026-09-16 14:06:46 +0200

    Initialize audit action integrations

--- app/wsgi.py
+++ app/wsgi.py
@@ -6,11 +6,13 @@ from node_actions import init_node_actions
 from config_transfer import init_config_transfer
 from failover_test import init_failover_test
 from audit_log import init_audit_log
+from audit_integration import init_audit_integration
 
 init_eventlog(core)
 init_cluster_validation(core)
 init_statistics(core)
 init_audit_log(core)
+init_audit_integration(core)
 init_node_actions(core)
 init_config_transfer(core)
 init_failover_test(core)
