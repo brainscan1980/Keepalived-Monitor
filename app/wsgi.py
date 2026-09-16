630d82e4949a30974313f62043ca28ef9d1f6988
Matthias Hester <72701890+brainscan1980@users.noreply.github.com>
2026-09-16 13:11:36 +0200

    Enable safe node action guard

--- app/wsgi.py
+++ app/wsgi.py
@@ -2,8 +2,10 @@ import app as core
 from eventlog import init_eventlog
 from cluster_integration import init_cluster_validation
 from statistics import init_statistics
+from node_actions import init_node_actions
 
 init_eventlog(core)
 init_cluster_validation(core)
 init_statistics(core)
+init_node_actions(core)
 app=core.app
